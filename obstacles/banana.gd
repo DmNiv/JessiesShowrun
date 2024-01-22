9200a423f79c8c268fc047089d4dfe6c4d4062e3
Pedro Castro <pedro7mirand@gmail.com>
2024-01-22 01:54:12 -0300

    toin oin oin oin

--- obstacles/banana.gd
+++ obstacles/banana.gd
@@ -4,10 +4,18 @@ extends StaticBody2D
 
 func _on_area_2d_body_entered(body):
 	$Sprite2D.visible = true
+	
 
 
 func _on_area_2d_2_body_entered(body):
 	print("toioioioinnn")
 	body.get_node("Sprite2D").rotate(180*PI/180)
 	body.slowDown()
+	$AudioStreamPlayer2D.play()
+	visible = false
+	
+
+
+
+func _on_audio_stream_player_2d_finished():
 	queue_free()
