df8cf79aacf052bc5d6a557627ce5965f0b001f8
Pedro Castro <pedro7mirand@gmail.com>
2024-01-25 23:46:06 -0300

    SPRITES

--- player/jessie.gd
+++ player/jessie.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const maxSpeed = 700.0
-const acceleration = 10
-const JUMP_VELOCITY = -500.0
+const maxSpeed = 900.0
+const acceleration = 20
+const JUMP_VELOCITY = -600.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var sliding = false
@@ -12,23 +12,21 @@ var pontuacao = 0
 
 
 func slowDown():
-	$AnimationPlayer.play("recover")
+	#$AnimationPlayer.play("recover")
 	velocity.x = 0
 
 func slide():
 	sliding = true
-	if velocity.x > 500:
-		velocity.x = 800
-	elif velocity.x < -500:
-		velocity.x = -800
-	$Sprite2D.scale.y = 1.25
+	if velocity.x > 700:
+		velocity.x = 1000
+	elif velocity.x < -700:
+		velocity.x = -1000
 	$CollisionShape2D.scale.y = 0.5
 	
 	if is_on_floor():
 		pass
 func stand():
 	sliding = false
-	$Sprite2D.scale.y = 2.5
 	$CollisionShape2D.scale.y = 1
 	
 func hitBanana():
@@ -41,14 +39,18 @@ func hitPie():
 func _process(delta):
 	$Label.text = "Pontuação: " + str(pontuacao)
 	
-	
-	
-	
-
+func animate():
+	if direction < 0:
+		$Sprite2D.flip_h = true
+	elif direction > 0:
+		$Sprite2D.flip_h = false
+	if direction == 0:
+		$AnimationPlayer.play("idle")
+	else:
+		$AnimationPlayer.play("RESET")
 
 func _physics_process(delta):
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
@@ -57,19 +59,16 @@ func _physics_process(delta):
 	elif not Input.is_action_pressed("ui_control") and is_on_floor():
 		stand()
 
-	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("ui_a", "ui_d")
 	if direction > 0 and sliding == false:
 		velocity.x += acceleration
 	elif direction < 0 and sliding == false:
 		velocity.x -= acceleration
 	elif is_on_floor():
-		velocity.x = lerp(velocity.x, 0.0, 0.025)
+		velocity.x = lerp(velocity.x, 0.0, 0.05)
 	if sliding == false:
 		velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
 		
@@ -77,7 +76,9 @@ func _physics_process(delta):
 		get_tree().paused = true
 		$pauseMenu.show()
 
+	animate()
 	move_and_slide()
+	
 
 
 
